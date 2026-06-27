a8218a6e51835033567d932b7fd850b67e32b9f9
Satya Prakash <swadeshicreator@gmail.com>
2026-06-27 23:32:38 +0530

    added a 5 second timeout on all database calls

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = async () => {
     try {
+        mongoose.set('maxTimeMS', 5000);
         await mongoose.connect(process.env.MONGODB_URI);
+        
         console.log("[API] Mongodb connected successfully.");
     } catch (error) {
         console.log(process.env.MONGODB_URI);
